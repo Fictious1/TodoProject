d277b3251610a1fe2d3c7b1e0efb554ce564b8e6
Prasu <prasannadavidraaj@gmail.com>
2026-05-12 18:36:53 +0530

    First version of ToDo app

---
+++ todo.py
@@ -0,0 +1,40 @@
+tasks = []
+
+while True:
+    print("\n===== TO-DO LIST =====")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Delete Task")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == '1':
+        task = input("Enter task: ")
+        tasks.append(task)
+        print("Task Added!")
+
+    elif choice == '2':
+        print("\nYour Tasks:")
+        
+        if len(tasks) == 0:
+            print("No tasks found.")
+        else:
+            for i, task in enumerate(tasks):
+                print(i + 1, task)
+
+    elif choice == '3':
+        num = int(input("Enter task number to delete: "))
+        
+        if num <= len(tasks):
+            tasks.pop(num - 1)
+            print("Task Deleted!")
+        else:
+            print("Invalid task number.")
+
+    elif choice == '4':
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid choice.")
